bf7abbec2790807e2bc90e1e143ca7b744392ff5
Grégoire BESSAGNET <Gregoire@greg.local>
2017-03-19 16:54:05 +0100

    added index files to test text retrieving

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('index.txt', function (err, data){
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data.toString());
+});
+
+console.log("we are finished !")
